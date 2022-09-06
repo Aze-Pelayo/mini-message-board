4d1e84d246d15d312237961df98e7ef203ad64fb
Aze Pelayo <azriellepelayo@gmail.com>
2022-09-06 20:57:44 +0800

    Implement MVP based on TOP

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,42 @@
 var express = require('express');
 var router = express.Router();
 
+const messages = [
+  {
+    text: "Hi",
+    user: "Amando",
+    added: new Date()
+  },
+  {
+    text: "Hello",
+    user: "Ivan",
+    added: new Date()
+  }
+];
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { 
+    title: 'Mini Messageboard', 
+    messages: messages });
 });
 
+router.get('/new', function(req, res, next) {
+  res.render('form', {
+    title: "Send a message"
+  } )
+})
+
+router.post('/new', function(req, res,next) {
+  const messageAuthor = req.body.author;
+  const messageText = req.body.message;
+
+  messages.push({
+    text: messageText,
+    user: messageAuthor,
+    added: new Date()
+  });
+
+  res.redirect('/');
+})
 module.exports = router;
